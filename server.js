ce1c615acaca7638e51e3fcc981e8bb92f713593
Kapil Jangra <kapiljangra2005@gmail.com>
2026-05-02 10:29:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 // Socket.io setup
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'https://findit-frontend-e141.vercel.app/'
+    origin: process.env.FRONTEND_URL || 'https://findit-frontend-e141.vercel.app/',
     methods: ['GET', 'POST']
   }
 });
